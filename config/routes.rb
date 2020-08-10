e2bec62581f0fcd294b29c69cdadf5a58612c8f6
aika.kayamori <kay.indig029@gmail.com>
2020-08-10 02:16:42 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :tasks, :users, only: [:index, :show, :new, :create]
 end
