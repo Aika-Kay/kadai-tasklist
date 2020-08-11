658809219c058cab734b649e0ce130b420915ef8
aika.kayamori <kay.indig029@gmail.com>
2020-08-11 03:32:03 +0000

    修正2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
+  before_action :require_user_logged_in, only: [:index]
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(5)
   end
 
   def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
   end
 
   def new
@@ -19,7 +15,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
