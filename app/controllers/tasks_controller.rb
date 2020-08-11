8027e3f5992533f9b0c9d2dd06b5a8eb8819ca06
aika.kayamori <kay.indig029@gmail.com>
2020-08-11 13:20:08 +0000

    修正5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = surrent_user.tasks.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def task_params
